0b503d0488171ff76ed41b1e6251afa370683b8b
Mitsukuni Sato <mitsukuni.sato@gmail.com>
2020-11-23 17:39:52 +0900

    Update setup.py

--- setup.py
+++ setup.py
@@ -33,7 +33,7 @@ setup(
     author="Phillip J. Eby",
     author_email="peak@eby-sarna.com",
     license="ZPL 2.1",
-    url="http://pypi.python.org/pypi/PEAK-Rules",
+    url="https://pypi.python.org/pypi/PEAK-Rules",
     download_url = "http://peak.telecommunity.com/snapshots/",
     test_suite = 'test_rules', tests_require='Importing',
     packages = PACKAGES,
